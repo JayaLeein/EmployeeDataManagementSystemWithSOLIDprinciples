7202d2b9f19e80926eaafb8d57f768d15ba29245
JingYi Li <li000848@algonquinlive.com>
2024-01-26 07:53:51 -0500

    first commit

---
+++ src/main/java/com/mycompany/asg1/Asg1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asg1;
+
+/**
+ *
+ * @author jayalee
+ */
+public class Asg1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
